dc90c34418776f89267ecf2298d9eb1ee13af792
LABORATORIO FELIPE SEGOVIA <laboratorio.fs@ad.uisek.edu.ec>
2026-07-21 09:47:50 -0500

    added theme but its not functional because the styles are hardcoded

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; 
 import App from './App.jsx';
 import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { themeOptions } from './theme/theme.js';
+
+const theme = createTheme(themeOptions);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/theme/theme.js
@@ -0,0 +1,36 @@
+export const themeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#ff5300',
+    },
+    secondary: {
+      main: '#ffffff',
+      dark: '#8c8989',
+      light: '#b5b4b4',
+    },
+    warning: {
+      main: '#bd181b',
+      light: '#bf282b',
+      contrastText: '#e8e1e1',
+    },
+    success: {
+      main: '#00c708',
+    },
+    info: {
+      main: '#0515e4',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto',
+    h1: {
+      fontFamily: 'Droid Serif',
+    },
+    h2: {
+      fontFamily: 'Droid Serif',
+    },
+    body1: {
+      fontFamily: 'Droid Serif',
+    },
+  },
+};
